389a1458dca4b170125cc5c4cad27c947dcf0677
andre <andremorais0000@gmail.com>
2024-07-31 23:22:30 -0300

    added euro conversion

--- scripts.js
+++ scripts.js
@@ -1,22 +1,40 @@
 const convertButton = document.querySelector(".convert-button")
+const currencyText = document.querySelector(".currency-text")
 
-function convertValues () {
+
+
+function convertValues() {
 
     const valueToConvert = document.querySelector(".valuetoconvert").value
     const currencyToConvert = document.getElementById("currency-to-convert")
     const currencyConverted = document.getElementById("currency-converted")
 
     const dolarToday = 5.61
+    const euroToday = 6.13
 
-    convertValues = valueToConvert / dolarToday
+
+    if (currencyText.value == "dolar") {
+        currencyConverted.innerHTML = new Intl.NumberFormat('en-US',
+            {
+                style: 'currency', currency: 'USD'
+            }).format(valueToConvert / dolarToday)
+
+    }
+
+    if (currencyText.value == "euro") {
+        currencyConverted.innerHTML = new Intl.NumberFormat('de-DE',
+            {
+                style: 'currency', currency: 'EUR'
+            }).format(valueToConvert / euroToday)
+
+    }
+
 
     currencyToConvert.innerHTML = new Intl.NumberFormat('pt-BR',
-        { style: 'currency', currency: 'BRL' 
+        {
+            style: 'currency', currency: 'BRL'
         }).format(valueToConvert)
 
-    currencyConverted.innerHTML = new Intl.NumberFormat('en-US',
-        { style: 'currency', currency: 'USD' 
-        }).format(convertValues)
 
 }
 
